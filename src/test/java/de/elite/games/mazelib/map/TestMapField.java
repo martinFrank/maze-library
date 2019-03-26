8fcf676112d3855dc79dae8efb6901b4e1b1c559
martin.frank <martin.frank.private@gmail.com>
2019-03-26 15:41:06 +0100

    minor method renaming

--- src/test/java/de/elite/games/mazelib/map/TestMapField.java
+++ src/test/java/de/elite/games/mazelib/map/TestMapField.java
@@ -1,6 +1,7 @@
 package de.elite.games.mazelib.map;
 
 import de.elite.games.drawlib.Point;
+import de.elite.games.drawlib.Shape;
 import de.elite.games.mazelib.data.TestFieldData;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
@@ -17,8 +18,9 @@ public class TestMapField extends MazeMapField<TestFieldData, TestMapField, Test
         Color color = getData().isMarkedAsPath() ? Color.YELLOW : getData().isDeadEnd() ? Color.LIGHTGRAY : Color.WHITE;
         GraphicsContext gc = (GraphicsContext) graphics;
         gc.setFill(color);
-        double[] xs = getTransformed().getPoints().stream().mapToDouble(Point::getX).toArray();
-        double[] ys = getTransformed().getPoints().stream().mapToDouble(Point::getY).toArray();
+        Shape shape = getShape().getTransformed();
+        double[] xs = shape.getPoints().stream().mapToDouble(Point::getX).toArray();
+        double[] ys = shape.getPoints().stream().mapToDouble(Point::getY).toArray();
         int amount = Math.min(xs.length, ys.length);
         gc.fillPolygon(xs, ys, amount);
         for (TestMapEdge e : getEdges()) {
